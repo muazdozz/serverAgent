c659eb52701002d81cd133ccccb526adc0fe7fd2
Muaz Ali <muaz16@gmail.com>
2017-07-04 16:40:17 +0530

    Update serverAgent.js

--- serverAgent.js
+++ serverAgent.js
@@ -8,24 +8,41 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 const socket = require('socket.io-client');
 var Pusher = require('pusher');
+var mysql = require('mysql');
 
 // To convert from bytes to gigabytes
 const bytesToGigaBytes = 1024 * 1024 * 1024;
 // To specify the interval (in milliseconds)
 const intervalInMs = 10000;
 
-var pusher = new Pusher({
-  appId: '361321',
-  key: 'fa2c8fc73abb0c80ad50',
-  secret: '31d5fc9a09ad20a9a47a',
-  cluster: 'ap2',
-  encrypted: true
-});
-//pusher notification 
-pusher.trigger('my-channel', 'my-event', {
-  "message": "hello world"
-});
 
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Wonderful1",
+  database: "sitemonitoring"
+});
+con.connect(function(err) {
+	if (err) throw err;
+  		console.log("Connected!");
+  		var sql = "INSERT INTO serverData (platform) VALUES ('platform')";
+  		con.query(sql, function (err, result) {
+    if (err) throw err;
+    	console.log("1 record inserted");
+  	});
+ });
+
+// var pusher = new Pusher({
+//   appId: '361321',
+//   key: 'fa2c8fc73abb0c80ad50',
+//   secret: '31d5fc9a09ad20a9a47a',
+//   cluster: 'ap2',
+//   encrypted: true
+// });
+// //pusher notification 
+// pusher.trigger('my-channel', 'my-event', {
+//   "message": "hello world"
+// });
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -33,7 +50,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 4321;        // set our port
+var port = process.env.PORT || 4322;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -76,12 +93,68 @@ function getStaticData(callback){
 		});
 	});
 }
+function getRamData(callback){
+	return new Promise((resolve, reject)=>{
+		process.nextTick(()=>{
+			let ramData={};
+			si.mem().then(resp=>{
+				ramData.ramTotal = (resp.total/bytesToGigaBytes).toFixed(2);
+				ramData.ramUsed = (resp.used/bytesToGigaBytes).toFixed(2);
+				ramData.ramFree =(resp.free/bytesToGigaBytes).toFixed(2);
+
+				if (callback) { 
+					callback(ramData) 
+				}
+				    resolve(ramData);
+			});
+		});
+	});
+}
+function getCPU(callback){
+	return new Promise((resolve, reject)=>{
+		process.nextTick(()=>{
+			let cpuData={};
+			si.currentLoad().then(resp=>{
+				cpuData.cpu_avgload = resp.avgload;
+				cpuData.cpu_currentload = resp.currentload;
+				cpuData.cpu_currentload_user = resp.currentload_user;
+				cpuData.cpu_currentload_system =resp.currentload_system;
+
+				si.cpuTemperature().then(resp=>{
+					cpuData.cpuTemperature=resp.main;
+
+					if (callback) { 
+						callback(cpuData) 
+					}
+				    resolve(cpuData);
+				});
+			});
+		});
+	});
+}
+function getNetwork(callback){
+	return new Promise((resolve, reject)=>{
+		process.nextTick(()=>{
+			let networkData={};
+			si.networkStats().then(resp=>{
+				networkData.networkStats_Recieved_sec = (resp.rx_sec).toFixed(2);
+				networkData.networkStats_Transfered_sec = (resp.tx_sec).toFixed(2);
+				networkData.networkStats_Recieved_rx = (resp.rx).toFixed(2);
+				networkData.networkStats_Transfered_tx = (resp.tx).toFixed(2);
+
+				if (callback) { 
+					callback(networkData) 
+				}
+				    resolve(networkData);
+			});
+		});
+	});
+}
 
 function getDynamicData(callback){
 	return new Promise((resolve, reject) => {
 	  	process.nextTick(() => {
 	  		let dynamicData = {};
-	  		var interfaces ;
 			si.cpuTemperature().then(resp=>{
 				dynamicData.cpuTemperature=resp.main;
 				si.mem().then(resp=>{
@@ -122,6 +195,28 @@ router.get('/dynamicData', function(req, res) {
 	    	res.json(dynamicData);
 			});	
 });
+router.get('/ramData', function(req, res) {
+			getRamData(function(ramData){
+	    	res.json(ramData);
+			});	
+});
+
+router.get('/cpuData', function(req, res) {
+			getCPU(function(cpuData){
+	    	res.json(cpuData);
+			});	
+});
+
+router.get('/networkData', function(req, res) {
+			getNetwork(function(networkData){
+	    	res.json(networkData);
+			});	
+});
+
+router.post('/ramData',function(req,res){
+	var ramData = req.json(ramData);
+	
+});
 
 
 // more routes for our API will happen here
@@ -129,6 +224,10 @@ router.get('/dynamicData', function(req, res) {
 // all of our routes will be prefixed with /api
 app.use('/api', router);
 app.use('/dynamicData', router);
+app.use('/ramData', router);
+app.use('/cpuData', router);
+app.use('/networkData', router);
+
 
 // START THE SERVER
 // =============================================================================
